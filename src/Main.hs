f3c3ff425ee9d4c4ab739028292d37e2cb93b91c
Jean-Pierre Delouche <jdelouche@wanadoo.fr>
2015-04-09 05:38:22 -0700

    30

--- src/Main.hs
+++ src/Main.hs
@@ -4,4 +4,4 @@ module Main where
 -- | The main entry point.
 main :: IO ()
 primes n = [p | p <- [1..n], p `notElem` [a*b | a <- [2..n], b <- [2..n]]]
-main = print (primes 25)
+main = print (primes 30)
